a0f1e0db6a9846597b0a7fce8b94d209387fdaa7
Bassam Pervez <bassampervez@gmail.com>
2023-01-14 19:52:38 +0530

    Add files via upload

--- brrt.py
+++ brrt.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import random
 import math
 import numpy as np
+
 # PARAMETERS AND INITIAL SETTINGS
 
 START = (1, 1)
@@ -17,26 +18,27 @@ def line_intersects_circle(x1, y1, x2, y2, a, b, r):
 
     m = (y2 - y1) / (x2 - x1)
     b = y1 - m * x1
-    A = m**2 + 1
+    A = m ** 2 + 1
     B = 2 * (m * b - m * a - a)
-    C = a**2 + b**2 - 2 * b * a + r**2 - r**2
-    discriminant = B**2 - 4 * A * C
+    C = a ** 2 + b ** 2 - 2 * b * a + r ** 2 - r ** 2
+    discriminant = B ** 2 - 4 * A * C
     if discriminant < 0:
         # No intersection
-        return False    
+        return False
 
     # line intersects, but we care only about  intserction of LINE SEGMENT for checking the intersection with obstacles
- 
-   
+
     x_intersect = (-B + math.sqrt(discriminant)) / (2 * A)
     y_intersect = m * x_intersect + b
-    
+
     # Check if the intersection point is within the bounds of the line segment
-    if (x_intersect >= min(x1, x2) and x_intersect <= max(x1, x2)) and (y_intersect >= min(y1, y2) and y_intersect <= max(y1, y2)):
+    if (x_intersect >= min(x1, x2) and x_intersect <= max(x1, x2)) and (
+        y_intersect >= min(y1, y2) and y_intersect <= max(y1, y2)
+    ):
         return True
     else:
         return False
-    
+
 
 def check_in_circle(point, circles):
     for i in circles:
@@ -46,86 +48,100 @@ def check_in_circle(point, circles):
 
 
 def check_connectable(p1, p2, circle_obstacles):
-    
+
     for c in circle_obstacles:
-        if (line_intersects_circle(*p1,*p2,*c)):
+        if line_intersects_circle(*p1, *p2, *c):
             return False
 
-    return True   
+    return True
 
-def interpolate_second_if_big(p1,p2,max_dist):
+
+def interpolate_second_if_big(p1, p2, max_dist):
     p1 = np.array(p1)
     p2 = np.array(p2)
-    #return (p2[0],p2[1])
-    dir_vec = p2-p1
-    if (math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2) > max_dist):
-        p2 = p1 +  (dir_vec/(np.linalg.norm(dir_vec))) * 1
-    return (p2[0],p2[1])
+    # return (p2[0],p2[1])
+    dir_vec = p2 - p1
+    if math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) > max_dist:
+        p2 = p1 + (dir_vec / (np.linalg.norm(dir_vec))) * 1
+    return (p2[0], p2[1])
+
+
 def build_rrt(start, goal, iterations, max_dist, circle_obstacles):
     node_list1 = [start]
     node_list2 = [goal]
-    node_dict1 = {start:()}
-    node_dict2 = {goal:()}
+    node_dict1 = {start: ()}
+    node_dict2 = {goal: ()}
 
-    
     for i in range(iterations):
         while True:
             candidate1 = (random.uniform(0, 30), random.uniform(0, 30))
-            if not check_in_circle(candidate1,circle_obstacles):
-                sorted_list = sorted(node_dict1.keys(),key = lambda x:math.sqrt((candidate1[0]-x[0])**2 + (candidate1[1]-x[1])**2))
+            if not check_in_circle(candidate1, circle_obstacles):
+                sorted_list = sorted(
+                    node_dict1.keys(),
+                    key=lambda x: math.sqrt(
+                        (candidate1[0] - x[0]) ** 2 + (candidate1[1] - x[1]) ** 2
+                    ),
+                )
 
                 flag_no_connect = True
                 for i in sorted_list:
-                    if(check_connectable(i,candidate1,circle_obstacles)):
-                        candidate1 = interpolate_second_if_big(i,candidate1,max_dist)
-                        if check_in_circle(candidate1,circle_obstacles):
-                            break;
-                        node_dict1[candidate1]=[(i)]
-                        node_list1+=[(candidate1)]
+                    if check_connectable(i, candidate1, circle_obstacles):
+                        candidate1 = interpolate_second_if_big(i, candidate1, max_dist)
+                        if check_in_circle(candidate1, circle_obstacles):
+                            break
+                        node_dict1[candidate1] = [(i)]
+                        node_list1 += [(candidate1)]
 
                         flag_no_connect = False
                         break
                     else:
                         print("candidate not connectable")
-                
-                if not flag_no_connect:
-                    break;
 
+                if not flag_no_connect:
+                    break
 
         while True:
             candidate2 = (random.uniform(0, 30), random.uniform(0, 30))
-            if not check_in_circle(candidate2,circle_obstacles):
-                sorted_list = sorted(node_dict2.keys(),key = lambda x:math.sqrt((candidate2[0]-x[0])**2 + (candidate2[1]-x[1])**2))
+            if not check_in_circle(candidate2, circle_obstacles):
+                sorted_list = sorted(
+                    node_dict2.keys(),
+                    key=lambda x: math.sqrt(
+                        (candidate2[0] - x[0]) ** 2 + (candidate2[1] - x[1]) ** 2
+                    ),
+                )
 
                 flag_no_connect = True
                 for i in sorted_list:
-                    if(check_connectable(i,candidate2,circle_obstacles)):
-                        candidate2 = interpolate_second_if_big(i,candidate2,max_dist)
-                        if check_in_circle(candidate2,circle_obstacles):
-                            break;
+                    if check_connectable(i, candidate2, circle_obstacles):
+                        candidate2 = interpolate_second_if_big(i, candidate2, max_dist)
+                        if check_in_circle(candidate2, circle_obstacles):
+                            break
 
-                        node_dict2[candidate2]=[(i)]
-                        node_list2+=[(candidate2)]
+                        node_dict2[candidate2] = [(i)]
+                        node_list2 += [(candidate2)]
 
                         flag_no_connect = False
                         break
                     else:
                         print("candidate not connectable")
-                
+
                 if not flag_no_connect:
-                    break;
- 
-
-
+                    break
 
         for i in node_list1:
-            sorted_node_list2 = sorted(node_list2 ,key = lambda a:math.sqrt((i[0]-a[0])**2 + (i[1]-a[1])**2))
-            
-            for k in sorted_node_list2:
-                if check_connectable(i,k,circle_obstacles) and math.sqrt((k[0]-i[0])**2 + (k[1]-i[1])**2) < max_dist:
-                    return node_list1,node_list2                    
+            sorted_node_list2 = sorted(
+                node_list2,
+                key=lambda a: math.sqrt((i[0] - a[0]) ** 2 + (i[1] - a[1]) ** 2),
+            )
 
-    return node_list1,node_list2
+            for k in sorted_node_list2:
+                if (
+                    check_connectable(i, k, circle_obstacles)
+                    and math.sqrt((k[0] - i[0]) ** 2 + (k[1] - i[1]) ** 2) < max_dist
+                ):
+                    return node_list1, node_list2
+
+    return node_list1, node_list2
 
 
 # initialize the plot
@@ -133,9 +149,7 @@ plt.ion()  # turn on interactive mode
 fig, ax = plt.subplots()
 
 
-
-
-plt_circles = [plt.Circle((i[0],i[1]),i[2], color='g') for i in CIRCLE_OBSTACLES]
+plt_circles = [plt.Circle((i[0], i[1]), i[2], color="g") for i in CIRCLE_OBSTACLES]
 
 for p in plt_circles:
     ax.add_patch(p)
@@ -143,11 +157,10 @@ for p in plt_circles:
 # build the RRT
 
 
-
-tree1,tree2 = build_rrt(START, GOAL, MAX_ITER, MAX_DIST, CIRCLE_OBSTACLES)
+tree1, tree2 = build_rrt(START, GOAL, MAX_ITER, MAX_DIST, CIRCLE_OBSTACLES)
 
 # plot the points on the RRT incrementally
-for p1,p2 in zip(tree1,tree2):
+for p1, p2 in zip(tree1, tree2):
     ax.plot(p1[0], p1[1], "bo")
     ax.plot(p2[0], p2[1], "ro")
 
